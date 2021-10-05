261564cd537b60afd03355ba24ab9aa2ede4b590
arne-sas <r0802226@student.thomasmore.be>
2021-10-05 18:10:52 +0200

    Add Experiment 3

---
+++ code/Assignment_5/rest/src/test/java/hvl/dat250/restservice/RestserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package hvl.dat250.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
